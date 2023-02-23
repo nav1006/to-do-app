1e4f2e50b23b063f3b2cfc4613cf6067751fbea4
Navdeep Kumar <10navdeepkumar@gmail.com>
2023-02-23 12:23:37 +0530

    Implement the Exception handling, add clock and change theme

--- Main TODO/gui.py
+++ Main TODO/gui.py
@@ -1,6 +1,9 @@
 import functionsTODO as fn
 import PySimpleGUI as gui
+import time
 
+gui.theme('BluePurple')
+cloak = gui.Text('', key='cloak')
 label = gui.Text('Type in a to-do')
 input_box = gui.InputText(tooltip='Enter TO-DO', key='todo')
 add_button = gui.Button('Add')
@@ -10,17 +13,17 @@ edit_button = gui.Button('Edit')
 complete_button = gui.Button('Complete')
 exit_button = gui.Button('Exit')
 window = gui.Window('My TODO App',
-                    layout = [[label],
+                    layout = [[cloak],
+                              [label],
                               [input_box, add_button],
                               [list_box, edit_button, complete_button],
                               [exit_button]],
                     font=('Helvetica',20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window['cloak'].update(value=time.strftime('%b %d %Y, %H:%M:%S'))
+
     match event:
         case 'Add':
             todos = fn.get_todos()
@@ -30,21 +33,27 @@ while True:
             window['todos'].update(values=todos)
 
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
-            todos = fn.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            fn.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + '\n'
+                todos = fn.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                fn.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                gui.popup('Select Item', 'Please select a to-do first.', font=('Helvetica',20))
 
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = fn.get_todos()
-            todos.remove(todo_to_complete)
-            fn.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = fn.get_todos()
+                todos.remove(todo_to_complete)
+                fn.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                gui.popup('Select Item', 'Please select a to-do first.', font=('Helvetica', 20))
 
         case 'Exit':
             break
